6c44eb64f3b17f34d4a8d2bb008ea6e7266b65fd
Eric-Strady <strady60@gmail.com>
2019-02-20 12:14:35 +0100

    refactoring

--- js/slider.js
+++ js/slider.js
@@ -1,12 +1,18 @@
 $(function() {
 
-	let slider = {
-		
-		slides: $('#slides > div'),
-		length: $('#slides > div').length,
-		index: 0,
-		currentSlide: 0,
-		auto: '',
+	class Slider {
+		constructor(slidesBlockId, slides, slideId, markersBlockId, backward, forward) {
+			this.slidesBlockId = slidesBlockId;
+			this.slides = slides;
+			this.length = slides.length;
+			this.slideId = slideId;
+			this.markersBlockId = markersBlockId;
+			this.backward = backward;
+			this.forward = forward;
+			this.index = 0;
+			this.currentSlide = 0;
+			this.auto = '';
+		}
 
 		init() {
 			let firstSlide = this.slides.eq(this.currentSlide);
@@ -14,17 +20,17 @@ $(function() {
 			firstSlide.css('display', 'flex');
 
 			for (let i = 0; i < this.length; i++) {
-				$('#slider-marker').append('<a href="#" class="ml-2 mr-2" id="'+ i +'"></a>');
+				$(this.markersBlockId).append('<a href="#" class="ml-2 mr-2" id="'+ i +'"></a>');
 			}
+			this.markers = $(this.markersBlockId + ' a');
 
-			slider.markerSelected();
-			slider.stopAndStart();
-			slider.backwardEvent();
-			slider.forwardEvent();
-			slider.markerEvent();
-			slider.keyboardEvent();
-		},
-
+			this.markerSelected();
+			this.stopAndStart();
+			this.backwardEvent();
+			this.forwardEvent();
+			this.markerEvent();
+			this.keyboardEvent();
+		}
 
 		indexUp() {
 			this.index++;
@@ -32,7 +38,7 @@ $(function() {
 				this.index = 0;
 				this.currentSlide = this.length -1;
 			}
-		},
+		}
 
 		indexDown() {
 			this.index--;
@@ -40,69 +46,63 @@ $(function() {
 				this.index = this.length -1;
 				this.currentSlide = 0;
 			}
-		},
+		}
 
 		slidingLeft() {
 			if (this.index !== this.length -1) {
 				this.currentSlide = this.index+1;
 			}
 			
-			slider.showSlide();
-		},
+			this.showSlide();
+		}
 
 		slidingRight() {
-			if (this.index === 0) {
-				slider.showSlide();
-			}
-			else {
+			if (this.index !== 0) {
 				this.currentSlide = this.index-1;
-				slider.showSlide();
 			}
-		},
+
+			this.showSlide();
+		}
 
 		showSlide() {
-			$('#slide'+ this.currentSlide).fadeOut(300);
-			slider.markerSelected();
-			$('#slide'+ this.index).slideUp(500).delay(500).fadeIn(500);
-		},
+			$(this.slideId + this.currentSlide).fadeOut(300);
+			this.markerSelected();
+			$(this.slideId + this.index).slideUp(500).delay(500).fadeIn(500);
+		}
 
 		autoSlide() {
-			slider.auto = setInterval(function(){
+			this.auto = setInterval(function(){
 				slider.indexUp();
 				slider.slidingRight();
 			}, 5000);
-		},
+		}
 
 		stopSlide() {
 			clearInterval(this.auto);
-			$('#slider-marker a').css({
+			this.markers.css({
 				'background-color': '#BEBEBE',
 				'border': '3px solid #BEBEBE'
 			});
-		},
-
-		startSlide() {
-			slider.autoSlide();
-		},
+		}
 
 		stopAndStart() {
 
 			if (this.auto === '') {
-				slider.startSlide();
+				this.autoSlide();
 			}
 
-			$('#slides').hover(
+			this.slidesBlockId.hover(
 				function(){
 					slider.stopSlide();
 				},
 				function(){
 					slider.markerSelected();
-					slider.startSlide();
+					slider.autoSlide();
 			});
-		},
+		}
 
 		markerSelected() {
-			let markers = $('#slider-marker a');
+			let markers = this.markers;
 			let markerSelected = markers[this.index];
 
 			$(markers).css({
@@ -114,10 +114,10 @@ $(function() {
 				'background-color': '#D600FF',
 				'border': '3px solid #D600FF'
 			});
-		},
+		}
 
 		markerEvent() {
-			$('#slider-marker a').click(function(e) {
+			this.markers.click(function(e) {
 				e.preventDefault();
 				slider.stopSlide();
 				slider.currentSlide = slider.index;
@@ -126,23 +126,23 @@ $(function() {
 				slider.currentSlide = slider.index -1;
 				slider.startSlide();
 			});
-		},
+		}
 
 		backwardEvent() {
-			$('#backward').click(function(e) {
+			this.backward.click(function(e) {
 				e.preventDefault();
 				slider.indexDown();
 				slider.slidingLeft();
 			});
-		},
+		}
 
 		forwardEvent() {
-			$('#forward').click(function(e) {
+			this.forward.click(function(e) {
 				e.preventDefault();
 				slider.indexUp();
 				slider.slidingRight();
 			});
-		},
+		}
 
 		keyboardEvent() {
 			$('body').keydown(function(e) {
@@ -156,7 +156,16 @@ $(function() {
 				}
 			});
 		}
-	};
+	}
 
+
+	let slidesBlockId = $('#slides');
+	let slides = $('#slides > div');
+	let slideId = '#slide';
+	let markersBlockId = '#slider-marker';
+	let backward = $('#backward');
+	let forward = $('#forward');
+
+	const slider = new Slider(slidesBlockId, slides, slideId, markersBlockId, backward, forward);
 	slider.init();
 });

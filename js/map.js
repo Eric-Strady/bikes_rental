1069b7d5fc2c0a61b4149845f29b98e556cf5786
Eric-Strady <strady60@gmail.com>
2019-02-23 18:15:04 +0100

    refactoring with Main.js

--- js/map.js
+++ js/map.js
@@ -1,61 +1,44 @@
-$(function() {
-
-	class Map {
-		constructor(id, lat, lng) {
-			this.id = id;
-			this.latitude = lat;
-			this.longitude = lng;
-			this.map = '';
-			this.dataFromJCDecaux = 'https://api.jcdecaux.com/vls/v1/stations?contract=Nantes&apiKey=3e1f17ee3d8f0b4e911b05f690af84c74891c3fc';
-		}
-
-		generateMap() {
-			this.map = L.map(this.id).setView([this.latitude, this.longitude], 13);
-			this.getLayer();
-			this.getApiData();
-			return this.map;
-		}
-
-		getLayer() {
-			L.tileLayer('//{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
-			    attribution: 'donn&eacute;es &copy; <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
-			    maxZoom: 18,
-			}).addTo(this.map);
-		}
-
-		getApiData() {
-			ajaxGet(this.dataFromJCDecaux, function(response) {
-				let stations = JSON.parse(response);
-				console.log(stations);
-				map.generateMarkers(stations);
-			});
-		}
-
-		generateMarkers(stations) {
-			stations.forEach(station => {
-				let stationLat = station.position.lat;
-				let stationLng = station.position.lng;
-				let stationAdr = station.address;
-				let stationBikeStands = station.available_bike_stands;
-				let stationAvailableBikes = station.available_bikes;
-
-				let marker = L.marker([stationLat, stationLng])
-					.addTo(this.map)
-					.on('click', addData);
-
-				function addData() {
-					$('#help').hide();
-					$('#rental_form').fadeIn(1000);
-					$('#address span').text(stationAdr);
-					$('#bikeStands span').text(stationBikeStands);
-					$('#availableBikes span').text(stationAvailableBikes);
-				}
-			});
-		}
+class Map {
+	constructor(id, lat, lng) {
+		this.id = id;
+		this.latitude = lat;
+		this.longitude = lng;
+		this.map = '';
+		this.stations = '';
 	}
 
-	$('#rental_form').hide();
+	init() {
+		this.map = L.map(this.id).setView([this.latitude, this.longitude], 13);
+		this.getLayer();
+		return this.map;
+	}
 
-	const map = new Map('mapid', 47.217894, -1.552875);
-	map.generateMap();
-});
+	getLayer() {
+		L.tileLayer('//{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+		    attribution: 'donn&eacute;es &copy; <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
+		    maxZoom: 18,
+		}).addTo(this.map);
+	}
+
+	generateMarkers(stations) {
+		stations.forEach(station => {
+			let stationLat = station.position.lat;
+			let stationLng = station.position.lng;
+			let stationAdr = station.address;
+			let stationBikeStands = station.available_bike_stands;
+			let stationAvailableBikes = station.available_bikes;
+
+			let marker = L.marker([stationLat, stationLng])
+				.addTo(this.map)
+				.on('click', addData);
+
+			function addData() {
+				$('#help').hide();
+				$('#rental_form').fadeIn(1000);
+				$('#address span').text(stationAdr);
+				$('#bikeStands span').text(stationBikeStands);
+				$('#availableBikes span').text(stationAvailableBikes);
+			}
+		});
+	}
+}
--- js/slider.js
+++ js/slider.js
@@ -1,171 +1,86 @@
-$(function() {
+class Slider {
+	constructor(slides, slideId, markersBlockId) {
+		this.slides = slides;
+		this.length = slides.length;
+		this.slideId = slideId;
+		this.markersBlockId = markersBlockId;
+		this.index = 0;
+		this.currentSlide = 0;
+		this.auto = '';
+	}
 
-	class Slider {
-		constructor(slidesBlockId, slides, slideId, markersBlockId, backward, forward) {
-			this.slidesBlockId = slidesBlockId;
-			this.slides = slides;
-			this.length = slides.length;
-			this.slideId = slideId;
-			this.markersBlockId = markersBlockId;
-			this.backward = backward;
-			this.forward = forward;
+	init() {
+		let firstSlide = this.slides.eq(this.currentSlide);
+		this.slides.hide();
+		firstSlide.css('display', 'flex');
+
+		for (let i = 0; i < this.length; i++) {
+			$(this.markersBlockId).append('<a href="#" class="ml-2 mr-2" id="'+ i +'"></a>');
+		}
+		this.markers = $(this.markersBlockId + ' a');
+
+		this.markerSelected();
+	}
+
+	indexUp() {
+		this.index++;
+		if (this.index > this.length -1) {
 			this.index = 0;
-			this.currentSlide = 0;
-			this.auto = '';
-		}
-
-		init() {
-			let firstSlide = this.slides.eq(this.currentSlide);
-			this.slides.hide();
-			firstSlide.css('display', 'flex');
-
-			for (let i = 0; i < this.length; i++) {
-				$(this.markersBlockId).append('<a href="#" class="ml-2 mr-2" id="'+ i +'"></a>');
-			}
-			this.markers = $(this.markersBlockId + ' a');
-
-			this.markerSelected();
-			this.stopAndStart();
-			this.backwardEvent();
-			this.forwardEvent();
-			this.markerEvent();
-			this.keyboardEvent();
-		}
-
-		indexUp() {
-			this.index++;
-			if (this.index > this.length -1) {
-				this.index = 0;
-				this.currentSlide = this.length -1;
-			}
-		}
-
-		indexDown() {
-			this.index--;
-			if (this.index < 0) {
-				this.index = this.length -1;
-				this.currentSlide = 0;
-			}
-		}
-
-		slidingLeft() {
-			if (this.index !== this.length -1) {
-				this.currentSlide = this.index+1;
-			}
-			
-			this.showSlide();
-		}
-
-		slidingRight() {
-			if (this.index !== 0) {
-				this.currentSlide = this.index-1;
-			}
-
-			this.showSlide();
-		}
-
-		showSlide() {
-			$(this.slideId + this.currentSlide).fadeOut(300);
-			this.markerSelected();
-			$(this.slideId + this.index).slideUp(500).delay(500).fadeIn(500);
-		}
-
-		autoSlide() {
-			this.auto = setInterval(function(){
-				slider.indexUp();
-				slider.slidingRight();
-			}, 5000);
-		}
-
-		stopSlide() {
-			clearInterval(this.auto);
-			this.markers.css({
-				'background-color': '#BEBEBE',
-				'border': '3px solid #BEBEBE'
-			});
-		}
-
-		stopAndStart() {
-
-			if (this.auto === '') {
-				this.autoSlide();
-			}
-
-			this.slidesBlockId.hover(
-				function(){
-					slider.stopSlide();
-				},
-				function(){
-					slider.markerSelected();
-					slider.autoSlide();
-			});
-		}
-
-		markerSelected() {
-			let markers = this.markers;
-			let markerSelected = markers[this.index];
-
-			$(markers).css({
-				'background-color': 'white',
-				'border': '3px solid #9600B2'
-			});
-
-			$(markerSelected).css({
-				'background-color': '#D600FF',
-				'border': '3px solid #D600FF'
-			});
-		}
-
-		markerEvent() {
-			this.markers.click(function(e) {
-				e.preventDefault();
-				slider.stopSlide();
-				slider.currentSlide = slider.index;
-				slider.index = $(this).attr('id');
-				slider.showSlide();
-				slider.currentSlide = slider.index -1;
-				slider.startSlide();
-			});
-		}
-
-		backwardEvent() {
-			this.backward.click(function(e) {
-				e.preventDefault();
-				slider.indexDown();
-				slider.slidingLeft();
-			});
-		}
-
-		forwardEvent() {
-			this.forward.click(function(e) {
-				e.preventDefault();
-				slider.indexUp();
-				slider.slidingRight();
-			});
-		}
-
-		keyboardEvent() {
-			$('body').keydown(function(e) {
-				if (e.which === 37) {
-					slider.indexDown();
-					slider.slidingLeft();
-				}
-				else if (e.which === 39) {
-					slider.indexUp();
-					slider.slidingRight();
-				}
-			});
+			this.currentSlide = this.length -1;
 		}
 	}
 
+	indexDown() {
+		this.index--;
+		if (this.index < 0) {
+			this.index = this.length -1;
+			this.currentSlide = 0;
+		}
+	}
 
-	let slidesBlockId = $('#slides');
-	let slides = $('#slides > div');
-	let slideId = '#slide';
-	let markersBlockId = '#slider-marker';
-	let backward = $('#backward');
-	let forward = $('#forward');
+	slidingLeft() {
+		if (this.index !== this.length -1) {
+			this.currentSlide = this.index+1;
+		}
+	}
 
-	const slider = new Slider(slidesBlockId, slides, slideId, markersBlockId, backward, forward);
-	slider.init();
-});
+	slidingRight() {
+		if (this.index !== 0) {
+			this.currentSlide = this.index-1;
+		}
+	}
+
+	showSlide() {
+		$(this.slideId + this.currentSlide).fadeOut(300);
+		this.markerSelected();
+		$(this.slideId + this.index).slideUp(500).delay(500).fadeIn(500);
+	}
+
+	autoSlide() {
+		this.indexUp();
+		this.slidingRight();
+		this.showSlide();
+	}
+
+	rewindSlide() {
+		this.indexDown();
+		this.slidingLeft();
+		this.showSlide();
+	}
+
+	markerSelected() {
+		let markers = this.markers;
+		let markerSelected = markers[this.index];
+
+		$(markers).css({
+			'background-color': 'white',
+			'border': '3px solid #9600B2'
+		});
+
+		$(markerSelected).css({
+			'background-color': '#D600FF',
+			'border': '3px solid #D600FF'
+		});
+	}
+}
+

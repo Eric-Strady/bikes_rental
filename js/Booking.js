f312e07efe2c850d354fd543670b029646b9cc53
Eric-Strady <strady60@gmail.com>
2019-02-26 11:32:35 +0100

    create Booking class

---
+++ js/Booking.js
@@ -0,0 +1,8 @@
+class Booking {
+	constructor(stationNumber, stationAddress, lastName, firstName) {
+		this.stationNumber = stationNumber;
+		this.stationAddress = stationAddress;
+		this.lastName = lastName;
+		this.firstName = firstName;
+	}
+}
